2366a745b082dfa4e46a6b9e32fad28566c97d1d
K <kihyuk.sung@outlook.kr>
2023-07-28 11:23:02 +0900

    BindingResult1
    - 에러 객체 대신 BindingResult 사용
    - thymeleaf 기능 사용 (errorclass, fields...)

--- validation/src/main/kotlin/hello/itemservice/web/validation/ValidationItemControllerV2.kt
+++ validation/src/main/kotlin/hello/itemservice/web/validation/ValidationItemControllerV2.kt
@@ -8,6 +8,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.util.StringUtils
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
@@ -40,29 +43,26 @@ class ValidationItemControllerV2(
     }
 
     @PostMapping("/add")
-    fun addItem(@ModelAttribute("item") item: ItemDto, redirectAttributes: RedirectAttributes, model: Model): String {
-        val errors = buildMap {
-            if (!StringUtils.hasText(item.itemName)) {
-                put("itemName", "상품 이름은 필수입니다.")
-            }
-            if (item.price == null || item.price < 1000 || item.price > 1_000_000) {
-                put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.")
-            }
-            if (item.quantity == null || item.quantity < 1 || item.quantity >= 9_999) {
-                put("quantity", "수량은 최대 9,999 까지 허용합니다.")
-            }
+    fun addItemV1(@ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
+        if (!StringUtils.hasText(item.itemName)) {
+            bindingResult.addError(FieldError("item", "itemName", "상품 이름은 필수입니다."))
+        }
+        if (item.price == null || item.price < 1000 || item.price > 1_000_000) {
+            bindingResult.addError(FieldError("item", "price", "가격은 1,000 ~ 1,000,000 까지 허용합니다."))
+        }
+        if (item.quantity == null || item.quantity < 1 || item.quantity >= 9_999) {
+            bindingResult.addError(FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."))
+        }
 
-            if (item.price != null && item.quantity != null) {
-                val resultPrice = item.price * item.quantity
-                if (resultPrice < 10_000) {
-                    put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = $resultPrice")
-                }
+        if (item.price != null && item.quantity != null) {
+            val resultPrice = item.price * item.quantity
+            if (resultPrice < 10_000) {
+                bindingResult.addError(ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = $resultPrice"))
             }
         }
 
-        if (errors.isNotEmpty()) {
-            log.info("errors = {}", errors)
-            model.addAttribute("errors", errors)
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult)
             return "validation/v2/addForm"
         }
 
