ed1198cdf5d3b4610cd14d4d4327627a1fd549b1
K <kihyuk.sung@outlook.kr>
2023-08-06 08:09:16 +0900

    Bean Validation - 스프링 적용
    - 글로벌 validator 설정 제거해야 한다.

--- validation/src/main/kotlin/hello/itemservice/web/validation/ValidationItemControllerV3.kt
+++ validation/src/main/kotlin/hello/itemservice/web/validation/ValidationItemControllerV3.kt
@@ -1,20 +1,14 @@
 package hello.itemservice.web.validation
 
 import hello.itemservice.domain.item.IdGenerator
-import hello.itemservice.domain.item.dto.ItemDto
 import hello.itemservice.domain.item.ItemRepository
-import hello.itemservice.domain.item.ItemValidator
+import hello.itemservice.domain.item.dto.ItemDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.util.StringUtils
 import org.springframework.validation.BindingResult
-import org.springframework.validation.FieldError
-import org.springframework.validation.ObjectError
-import org.springframework.validation.ValidationUtils
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
@@ -23,12 +17,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes
 class ValidationItemControllerV3(
     private val itemRepository: ItemRepository,
     private val idGenerator: IdGenerator,
-    private val itemValidator: ItemValidator,
 ) {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
-
-    @InitBinder
-    fun init(dataBinder: WebDataBinder): Unit = dataBinder.addValidators(itemValidator)
 
     @GetMapping
     fun items(model: Model): String {
@@ -50,143 +40,8 @@ class ValidationItemControllerV3(
         return "validation/v3/addForm"
     }
 
-//    @PostMapping("/add")
-    fun addItemV1(@ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
-        if (!StringUtils.hasText(item.itemName)) {
-            bindingResult.addError(FieldError("item", "itemName", "상품 이름은 필수입니다."))
-        }
-        if (item.price == null || item.price!! < 1000 || item.price!! > 1_000_000) {
-            bindingResult.addError(FieldError("item", "price", "가격은 1,000 ~ 1,000,000 까지 허용합니다."))
-        }
-        if (item.quantity == null || item.quantity!! < 1 || item.quantity!! >= 9_999) {
-            bindingResult.addError(FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."))
-        }
-
-        if (item.price != null && item.quantity != null) {
-            val resultPrice = item.price!! * item.quantity!!
-            if (resultPrice < 10_000) {
-                bindingResult.addError(ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = $resultPrice"))
-            }
-        }
-
-        if (bindingResult.hasErrors()) {
-            log.info("errors = {}", bindingResult)
-            return "validation/v3/addForm"
-        }
-
-        val (id) = itemRepository.save(item.toItem(idGenerator.next()))
-        redirectAttributes.addAttribute("itemId", id)
-        redirectAttributes.addAttribute("status", true)
-        return "redirect:/validation/v3/items/{itemId}"
-    }
-
-//    @PostMapping("/add")
-    fun addItemV2(@ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
-        if (!StringUtils.hasText(item.itemName)) {
-            bindingResult.addError(FieldError("item", "itemName", item.itemName, false, null, null, "상품 이름은 필수입니다."))
-        }
-        if (item.price == null || item.price!! < 1000 || item.price!! > 1_000_000) {
-            bindingResult.addError(FieldError("item", "price", item.price, false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."))
-        }
-        if (item.quantity == null || item.quantity!! < 1 || item.quantity!! >= 9_999) {
-            bindingResult.addError(FieldError("item", "quantity", item.quantity, false, null, null, "수량은 최대 9,999 까지 허용합니다."))
-        }
-
-        if (item.price != null && item.quantity != null) {
-            val resultPrice = item.price!! * item.quantity!!
-            if (resultPrice < 10_000) {
-                bindingResult.addError(ObjectError("item", null, null,"가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = $resultPrice"))
-            }
-        }
-
-        if (bindingResult.hasErrors()) {
-            log.info("errors = {}", bindingResult)
-            return "validation/v3/addForm"
-        }
-
-        val (id) = itemRepository.save(item.toItem(idGenerator.next()))
-        redirectAttributes.addAttribute("itemId", id)
-        redirectAttributes.addAttribute("status", true)
-        return "redirect:/validation/v3/items/{itemId}"
-    }
-
-//    @PostMapping("/add")
-    fun addItemV3(@ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
-        if (!StringUtils.hasText(item.itemName)) {
-            bindingResult.addError(FieldError("item", "itemName", item.itemName, false, arrayOf("required.item.itemName"), null, null))
-        }
-        if (item.price == null || item.price!! < 1000 || item.price!! > 1_000_000) {
-            bindingResult.addError(FieldError("item", "price", item.price, false, arrayOf("range.item.price"), arrayOf(1_000, 1_000_000), null))
-        }
-        if (item.quantity == null || item.quantity!! < 1 || item.quantity!! >= 9_999) {
-            bindingResult.addError(FieldError("item", "quantity", item.quantity, false, arrayOf("max.item.quantity"), arrayOf(9_999), null))
-        }
-
-        if (item.price != null && item.quantity != null) {
-            val resultPrice = item.price!! * item.quantity!!
-            if (resultPrice < 10_000) {
-                bindingResult.addError(ObjectError("item", arrayOf("totalPriceMin"), arrayOf(10_000, resultPrice),null))
-            }
-        }
-
-        if (bindingResult.hasErrors()) {
-            log.info("errors = {}", bindingResult)
-            return "validation/v3/addForm"
-        }
-
-        val (id) = itemRepository.save(item.toItem(idGenerator.next()))
-        redirectAttributes.addAttribute("itemId", id)
-        redirectAttributes.addAttribute("status", true)
-        return "redirect:/validation/v3/items/{itemId}"
-    }
-
-//    @PostMapping("/add")
-    fun addItemV4(@ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
-
-        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required")
-
-        if (item.price == null || item.price!! < 1000 || item.price!! > 1_000_000) {
-            bindingResult.rejectValue("price", "range", arrayOf(1_000, 1_000_000), null)
-        }
-        if (item.quantity == null || item.quantity!! < 1 || item.quantity!! >= 9_999) {
-            bindingResult.rejectValue("quantity", "max", arrayOf(9_999), null)
-        }
-
-        if (item.price != null && item.quantity != null) {
-            val resultPrice = item.price!! * item.quantity!!
-            if (resultPrice < 10_000) {
-                bindingResult.reject("totalPriceMin", arrayOf(10_000, resultPrice), null)
-            }
-        }
-
-        if (bindingResult.hasErrors()) {
-            log.info("errors = {}", bindingResult)
-            return "validation/v3/addForm"
-        }
-
-        val (id) = itemRepository.save(item.toItem(idGenerator.next()))
-        redirectAttributes.addAttribute("itemId", id)
-        redirectAttributes.addAttribute("status", true)
-        return "redirect:/validation/v3/items/{itemId}"
-    }
-
-//    @PostMapping("/add")
-    fun addItemV5(@ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
-        itemValidator.validate(item, bindingResult)
-
-        if (bindingResult.hasErrors()) {
-            log.info("errors = {}", bindingResult)
-            return "validation/v3/addForm"
-        }
-
-        val (id) = itemRepository.save(item.toItem(idGenerator.next()))
-        redirectAttributes.addAttribute("itemId", id)
-        redirectAttributes.addAttribute("status", true)
-        return "redirect:/validation/v3/items/{itemId}"
-    }
-
     @PostMapping("/add")
-    fun addItemV6(@Validated @ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
+    fun addItem(@Validated @ModelAttribute("item") item: ItemDto, bindingResult: BindingResult, redirectAttributes: RedirectAttributes, model: Model): String {
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult)
             return "validation/v3/addForm"
