e30dacf3767760c3e8b6f9383fa3dec435d4fd94
K <kihyuk.sung@outlook.kr>
2023-08-16 08:28:53 +0900

    로그인 처리하기 - 직접 만든 세션 사용

--- login/src/main/kotlin/hello/itemservice/web/HomeController.kt
+++ login/src/main/kotlin/hello/itemservice/web/HomeController.kt
@@ -1,6 +1,9 @@
 package hello.itemservice.web
 
+import hello.itemservice.domain.member.Member
 import hello.itemservice.domain.member.MemberRepository
+import hello.itemservice.web.session.SessionManager
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.CookieValue
@@ -9,15 +12,26 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class HomeController(
     private val memberRepository: MemberRepository,
+    private val sessionManager: SessionManager,
 ) {
 
-    @GetMapping("/")
+//    @GetMapping("/")
     fun homeLogin(
         @CookieValue(name = "memberId", required = false)
         memberId: Long?,
         model: Model,
     ): String = memberId
         ?.let { memberRepository.findById(it) }
+        ?.let { model.addAttribute("member", it) }
+        ?.let { "loginHome" }
+        ?: "home"
+
+    @GetMapping("/")
+    fun homeLoginV2(
+        request: HttpServletRequest,
+        model: Model,
+    ): String = (sessionManager.getSession(request) as? Member)
+        ?.let { memberRepository.findById(it.id) }
         ?.let { model.addAttribute("member", it) }
         ?.let { "loginHome" }
         ?: "home"
--- login/src/main/kotlin/hello/itemservice/web/login/LoginController.kt
+++ login/src/main/kotlin/hello/itemservice/web/login/LoginController.kt
@@ -1,7 +1,9 @@
 package hello.itemservice.web.login
 
 import hello.itemservice.domain.login.LoginService
+import hello.itemservice.web.session.SessionManager
 import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping
 @Controller
 class LoginController(
     private val loginService: LoginService,
+    private val sessionManager: SessionManager,
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -21,7 +24,7 @@ class LoginController(
     fun loginForm(@ModelAttribute("loginForm") form: LoginForm): String = "login/loginForm"
 
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     fun login(
         @Validated @ModelAttribute("loginForm") form: LoginForm,
         bindingResult: BindingResult,
@@ -52,9 +55,45 @@ class LoginController(
         return "redirect:/"
     }
 
-    @PostMapping("logout")
+    @PostMapping("/login")
+    fun loginV2(
+        @Validated @ModelAttribute("loginForm") form: LoginForm,
+        bindingResult: BindingResult,
+        response: HttpServletResponse
+    ): String {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm"
+        }
+
+        log.info("form = {}", form)
+
+        val member = with(form) {
+            loginService.login(
+                loginId = loginId,
+                password = password,
+            )
+        }
+
+        if (member == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.")
+            return "login/loginForm"
+        }
+
+        // TODO: 로그인 성공 처리
+        sessionManager.createSession(member, response)
+
+        return "redirect:/"
+    }
+
+//    @PostMapping("logout")
     fun logout(response: HttpServletResponse): String {
         expireCookie(response, "memberId")
+        return "redirect:/"
+    }
+
+    @PostMapping("logout")
+    fun logoutV2(request: HttpServletRequest): String {
+        sessionManager.expire(request)
         return "redirect:/"
     }
 
